c728ffb7a309e069cd48fa49006c5d6099c33b41
Kseniia Kosonogova <k.kosonogova@xlab-game.com>
2024-03-07 15:37:37 +0300

    cooking app

---
+++ Models/Playlist.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+using System.Text.Json.Serialization;
+
+namespace Cooking.Models
+{
+    public class Playlist
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string userName { get; set; } = null!;
+
+        [BsonElement("items")]
+        [JsonPropertyName("items")]
+        public List<string> movieIds { get; set; } = null!;
+    }
+}
